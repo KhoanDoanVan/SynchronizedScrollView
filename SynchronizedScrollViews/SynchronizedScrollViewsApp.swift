78958595d205d018103add8f3b515317245a865c
khoanisdev <doanvankhoan124@gmail.com>
2024-11-14 16:33:03 +0700

    Initial Commit

---
+++ SynchronizedScrollViews/SynchronizedScrollViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynchronizedScrollViewsApp.swift
+//  SynchronizedScrollViews
+//
+//  Created by Đoàn Văn Khoan on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SynchronizedScrollViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
